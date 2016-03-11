0f5cb5bf5ead4be08d15c29e501b89cc3ac305e4
Brigitte Kozena <Brigittekozena@gmail.com>
2016-03-10 19:16:38 -0800

    Set up view files

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
+  $('#questionForm').on('submit', postQuestion);
+  $('#answerForm').on('submit', postAnswer);
+  $('#commentForm').on('submit', postComment);
+  $('#expandQuestion').on('click', showFormAndAnswers);
+  $('#expandAnswer').on('click', showFormAndComments);
 
 });
+
+var postQuestion = function(){
+  //do post to database
+  //get response
+  //put post onto homepage
+}
+
+
+var postAnswer = function(){
+  //do post to database
+  //get response
+  //put post onto homepage
+}
+
+
+var postComment = function(){
+  //do post to database
+  //get response
+  //put post onto homepage
+}
